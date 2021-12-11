ae79bfbb94f6d7911343f22b8bcfc35748eefafe
zlatanglor <oshovskiy.d@gmail.com>
2021-12-11 14:07:41 +0200

    add owl carusel, owl-dots. add hover

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        // autoplayTimeout:4000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
 });
